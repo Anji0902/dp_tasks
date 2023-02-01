88116a2f7481005c2bbe032aec386203482e4746
Anji0902 <angabai18@gmail.com>
2023-02-01 10:54:27 +0200

    some changes in index.php

--- layout/index.php
+++ layout/index.php
@@ -3,7 +3,7 @@
 
 <h1> Some content </h1>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde eaque nihil aperiam ullam temporibus. Voluptatibus maxime eos ipsum architecto nulla earum, asperiores quo doloremque harum error molestiae, vero, aliquid libero.
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione corporis, non ex obcaecati error autem recusandae, rerum est aspernatur quidem ipsum quibusdam dolorem unde deserunt ad eum. Minima, iure fuga.
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione corporis, non ex obcaecati  bcbcverror autem recusandae, rerum est aspernatur quidem ipsum quibusdam dolorem unde deserunt ad eum. Minima, iure fuga.
 
 
 
